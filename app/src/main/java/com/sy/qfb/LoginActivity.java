1f97974eee04b1edd7e49ce5683fcbbfc9cabb02
Yin Shen <shenyinabcd@msn.cn>
2017-06-08 13:51:13 +0800

    add some pages

---
+++ app/src/main/java/com/sy/qfb/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.sy.qfb;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by jshenf on 2017/6/8.
+ */
+
+public class LoginActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
---
+++ app/src/main/java/com/sy/qfb/MainActivity.java
@@ -0,0 +1,17 @@
+package com.sy.qfb;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by jshenf on 2017/6/8.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
