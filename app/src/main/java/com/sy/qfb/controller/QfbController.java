140a5985c03e20a36fc907cb38a3b0f93c1def7c
Yin Shen <shenyinabcd@msn.cn>
2017-09-13 21:32:04 +0800

    fixes

--- app/src/main/java/com/sy/qfb/controller/QfbController.java
+++ app/src/main/java/com/sy/qfb/controller/QfbController.java
@@ -229,7 +229,7 @@ public class QfbController {
 
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
-        calendar.set(Calendar.HOUR, 0);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
         calendar.set(Calendar.MINUTE, 0);
         calendar.set(Calendar.SECOND, 0);
         calendar.set(Calendar.MILLISECOND, 0);
